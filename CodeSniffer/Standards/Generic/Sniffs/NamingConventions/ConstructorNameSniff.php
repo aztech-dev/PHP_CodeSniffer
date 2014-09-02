af6b7592e4e9f193a4fa8629aea2a888799214f9
Thibaud Fabre <thibaud@aztech.io>
2014-09-02 15:58:30 +0200

    Start ctor lookup at start of class, not of function

--- CodeSniffer/Standards/Generic/Sniffs/NamingConventions/ConstructorNameSniff.php
+++ CodeSniffer/Standards/Generic/Sniffs/NamingConventions/ConstructorNameSniff.php
@@ -117,8 +117,10 @@ class Generic_Sniffs_NamingConventions_ConstructorNameSniff extends PHP_CodeSnif
     protected function hasRegularCtor(PHP_CodeSniffer_File $file, $stackPtr, $currScope)
     {
         $tokens = $file->getTokens();
-        $functionPtr = $stackPtr;
+        $functionPtr = $currScope;
         $scopeEndPtr = $tokens[$currScope]['scope_closer'];
+
+        var_dump($tokens[$stackPtr], $tokens[$currScope]);
 
         while ($functionPtr = $file->findNext(T_FUNCTION, $functionPtr + 1, $scopeEndPtr)) {
             $namePtr = $file->findNext(T_STRING, $functionPtr, $scopeEndPtr);
